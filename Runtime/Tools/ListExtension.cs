1fd0e0af5098a61ea1e6c054fed8797ff3aa0b2c
dncry <45026397+dncry@users.noreply.github.com>
2021-04-14 09:35:13 +0800

    add vector list  static extension

---
+++ Runtime/Tools/ListExtension.cs
@@ -0,0 +1,57 @@
+﻿/*----------------------------------------------------------------
+** 项目名称: _Tools
+** 文件名称：ListExtension
+** 创 建 者：万硕
+** 创建时间：2021年04月14日 星期三 09:19
+** 文件版本：V1.0.0
+* ===============================================================
+** 功能描述：
+**		
+**
+**----------------------------------------------------------------*/
+
+
+using System;
+using System.Collections.Generic;
+
+namespace WS.Auto
+{
+    public static class ListExtension
+    {
+        public static List<T> FluentAdd<T>(this List<T> list, T item)
+        {
+            list.Add(item);
+            return list;
+        }
+
+        public static List<T> FluentClear<T>(this List<T> list)
+        {
+            list.Clear();
+            return list;
+        }
+
+        public static List<T> FluentForEach<T>(this List<T> list, Action<T> action)
+        {
+            list.ForEach(action);
+            return list;
+        }
+
+        public static List<T> FluentInsert<T>(this List<T> list, int index, T item)
+        {
+            list.Insert(index, item);
+            return list;
+        }
+
+        public static List<T> FluentRemoveAt<T>(this List<T> list, int index)
+        {
+            list.RemoveAt(index);
+            return list;
+        }
+
+        public static List<T> FluentReverse<T>(this List<T> list)
+        {
+            list.Reverse();
+            return list;
+        }
+    }
+}
---
+++ Runtime/Tools/VectorExtension.cs
@@ -0,0 +1,62 @@
+﻿/*----------------------------------------------------------------
+** 项目名称: _Tools
+** 文件名称：VectorExtension
+** 创 建 者：万硕
+** 创建时间：2021年04月14日 星期三 09:21
+** 文件版本：V1.0.0
+* ===============================================================
+** 功能描述：
+**		
+**
+**----------------------------------------------------------------*/
+
+
+namespace WS.Auto
+{
+    using UnityEngine;
+
+
+    public static class VectorExtension
+    {
+        public static Vector3 RotateWithAxis(this Vector3 source, Vector3 axis, float angle)
+        {
+            Quaternion q = Quaternion.AngleAxis(angle, axis); // 旋转系数
+            return q * source; // 返回目标点
+        }
+
+        /// <summary>
+        /// 世界坐标转UI绝对坐标
+        /// </summary>
+        /// <param name="worldPos"></param>
+        /// <param name="worldCamera"></param>
+        /// <param name="uiCanvas">Canvas画布</param>
+        /// <param name="uiCamera"></param>
+        /// <returns></returns>
+        public static Vector3 World2UI(this Vector3 worldPos, Camera worldCamera, RectTransform uiCanvas,
+            Camera uiCamera)
+        {
+            var screenPos = worldCamera.WorldToScreenPoint(worldPos);
+            if (RectTransformUtility.ScreenPointToWorldPointInRectangle(uiCanvas, screenPos, uiCamera,
+                out var uiPos))
+            {
+            }
+
+            return uiPos;
+        }
+
+        /// <summary>
+        /// 屏幕坐标转UI相对坐标
+        /// </summary>
+        /// <returns></returns>
+        public static Vector2 Screen2UI(this Vector2 screenPos, RectTransform uiCanvas, Camera uiCamera)
+        {
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                uiCanvas, screenPos, uiCamera,
+                out var uiPosition))
+            {
+            }
+
+            return uiPosition;
+        }
+    }
+}
