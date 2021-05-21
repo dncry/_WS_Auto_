8028d052f3a7b0ea0474bc4624f9886af99eec85
dncry <wan.shuo@qq.com>
2021-05-21 15:09:24 +0800

    add tools

---
+++ Runtime/Tools/RandomExtension.cs
@@ -0,0 +1,61 @@
+﻿/*----------------------------------------------------------------
+** Creator：万硕
+** Time：2021年05月21日 星期五 15:04
+----------------------------------------------------------------*/
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WS.Auto
+{
+    public static class RandomExtension
+    {
+        public static T GetRandomValue<T>(this List<T> group)
+        {
+            return group[Random.Range(0, group.Count)];
+        }
+
+        public static T GetRandomValue<T>(this T[] group)
+        {
+            return group[Random.Range(0, group.Length)];
+        }
+
+        public static List<T> GetRandomCountGroup<T>(this List<T> group, int count)
+        {
+            var groupLength = group.Count;
+
+            if (groupLength < count)
+            {
+                Debug.LogWarning("越界");
+                return null;
+            }
+
+            return GetRandomCountGroupWithList(group.ToList(),
+                groupLength - count);
+        }
+
+        public static List<T> GetRandomCountGroup<T>(this T[] group, int count)
+        {
+            var groupLength = group.Length;
+
+            if (groupLength < count)
+            {
+                Debug.LogWarning("越界");
+                return null;
+            }
+
+            return GetRandomCountGroupWithList(group.ToList(), groupLength - count);
+        }
+
+        private static List<T> GetRandomCountGroupWithList<T>(List<T> group, int removeCount)
+        {
+            for (int i = 0; i < removeCount; i++)
+            {
+                group.Remove(group.GetRandomValue());
+            }
+
+            return group;
+        }
+    }
+}
---
+++ Runtime/Tools/TransformExtension.cs
@@ -0,0 +1,26 @@
+﻿/*----------------------------------------------------------------
+** Creator：万硕
+** Time：2021年05月21日 星期五 15:06
+----------------------------------------------------------------*/
+
+using UnityEngine;
+
+namespace WS.Auto
+{
+    public static class TransformExtension
+    {
+        public static void ResetToWorld(this Transform source)
+        {
+            source.position = Vector3.zero;
+            source.rotation = Quaternion.identity;
+            source.localScale = Vector3.one;
+        }
+
+        public static void ResetToLocal(this Transform source)
+        {
+            source.localPosition = Vector3.zero;
+            source.localRotation = Quaternion.identity;
+            source.localScale = Vector3.one;
+        }
+    }
+}
