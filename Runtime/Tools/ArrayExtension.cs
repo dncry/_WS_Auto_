6c979bb9b97729c3ab2181072887937989a1b74c
dncry <wan.shuo@qq.com>
2024-02-23 12:03:56 +0800

    save

---
+++ Runtime/Tools/ArrayExtension.cs
@@ -0,0 +1,15 @@
+﻿/*----------------------------------------------------------------
+** Creator：Dncry
+** Time：2024年02月23日 星期五 12:01
+----------------------------------------------------------------*/
+
+namespace _Tools
+{
+    public static class ArrayExtension
+    {
+        public static T GetLatest<T>(this T[] group, int value = 1)
+        {
+            return group[group.Length - value];
+        }
+    }
+}
